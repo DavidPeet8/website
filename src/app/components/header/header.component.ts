435e6348a2c1439fbf4568f9e879720bf6d39652
davipeet8 <davidpeetp@gmail.com>
2020-04-30 00:01:01 -0400

    Removed unneeded images, added typing animation

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,11 +8,18 @@ import { ContentService } from '@shared/services/content.service'
 })
 export class HeaderComponent implements OnInit {
 	data;
+  headerText:string[] = ['Developer.', 'Athlete.', 'Architect.'];
+  typewriterText:string;
+  headerTextWordIdx:number = 0;
+  headerTextIdx:number = 0;
 
-  constructor(private contentService: ContentService) { }
+  constructor(private contentService: ContentService) {
+    this._typingCallBack = this._typingCallBack.bind(this);
+  }
 
   ngOnInit(): void {
   	this.data = this.contentService.getData(['header']);
+    this._typingCallBack();
   }
 
   getBackgroundImg() : Object
@@ -20,6 +27,29 @@ export class HeaderComponent implements OnInit {
   	return {
   		'background-image': this.data.imgPath
   	};
+  }
+
+  _typingCallBack() : void 
+  {
+    let timeoutConstant: number = 0;
+
+    this.headerTextIdx++;
+    if (this.headerTextIdx == this.headerText[this.headerTextWordIdx].length) 
+    {
+      this.headerTextIdx = 0;
+      this.headerTextWordIdx++;
+    }
+    if (this.headerTextWordIdx == this.headerText.length) 
+    {
+      this.headerTextWordIdx = 0;
+    }
+    if(this.headerTextIdx == this.headerText[this.headerTextWordIdx].length - 1)
+    { 
+      timeoutConstant = 1000; 
+    }
+
+    this.typewriterText = this.headerText[this.headerTextWordIdx].slice(0, this.headerTextIdx + 1);
+    setTimeout(this._typingCallBack, Math.random() * 180 + 100 + timeoutConstant, this);
   }
 
 }
