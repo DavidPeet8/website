2692c1692f23663fc3261c377ffd6a0a7bfeac89
davipeet8 <davidpeetp@gmail.com>
2020-05-02 13:21:42 -0400

    Added preview pic and added Google Analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
+declare let gtag:Function;
 
 @Component({
   selector: 'app-root',
@@ -8,7 +11,20 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'website';
 
-  ngOnInit(): void {
+  constructor(private router:Router) 
+  {
+  	 this.router.events.subscribe(event => {
+
+      if (event instanceof NavigationEnd) {
+      	console.log("I was supposed to do something useful")
+        gtag('set', 'page', event.urlAfterRedirects);
+        gtag('send', 'pageview');
+      }
+  	});
+  }
+
+  ngOnInit(): void 
+  {
   	console.log("App component was initialized")
   }
 }
