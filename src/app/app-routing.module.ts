327c399a18af2bb0e50b0bc1fa7f73a635cf1b67
davipeet8 <davidpeetp@gmail.com>
2021-01-02 13:05:05 -0500

    Bug fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
-import { ContentComponent } from '@content-list/components/content/content.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		loadChildren: () => import('@content-list/content-list.module').then(mod => mod.ContentListModule)
+	}
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes, {
