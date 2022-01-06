3bc371625313a0805162b43295089caea09e14ea
David Peet <davidpeet@tutanota.com>
2022-01-06 09:16:47 -0500

    Updated Open Graph tags & gitignore

--- src/components/helmet.js
+++ src/components/helmet.js
@@ -7,8 +7,13 @@ class PageHelmet extends React.Component {
 			<Helmet htmlAttributes={{ lang: 'en', }}>
 				<meta charSet="utf-8" />
 				<meta http-equiv="content-language" content="en-us" />
-				<meta name="description" content="Personal website for David Peet. Check out my software development projects!" />
+				<meta name="description" content="Hi, I'm David - I'm a software engineer and this is my website! Come check out my development projects!" />
 				<meta name="author" content="David Peet"></meta>
+				<meta property="og:image" content="../images/opengraph.png" />
+				<meta property="og:type" content="website" />
+				<meta property="og:url" content="https://website.davidpeet.me/" />
+				<meta property="og:title" content="David Peet" />
+				<meta property="og:description" content="Hi, I'm David - I'm a software engineer and this is my website! Come check out my development projects!" />
 				<title>David Peet</title>
 			</Helmet>
 		);
